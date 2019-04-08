5eb718b01ba727400822df58f9bb87b1ed98d6ea
StukaloAV <alexandrst@wizardsdev.com>
2019-04-08 19:57:36 +0300

    set db connect

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('DB connected'));
+
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+   console.log(`Server listening on PORT ${PORT}`)
+});
